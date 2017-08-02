690ec64706ba5c97c50118162767c70be6c95697
Brunno dos Santos <squiter85@gmail.com>
2017-08-02 20:33:56 -0300

    Create a `signout` route

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -19,6 +19,13 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> put_flash(:info, "See u' later")
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp signin(conn, changeset) do
     case insert_or_update_user(changeset) do
       {:ok, user} ->
